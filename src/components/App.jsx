d92a59b4bdab36c5f3c874b5e14da08940ffd2cf
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-01-16 00:12:06 +0200

    add APP settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import { Profile } from './Profile/Profile';
+
+import { Statistics } from './Statistics/Statistics';
+import data from './data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile />
+      <Statistics title="Upload stats" data={data} />
+    </>
   );
 };
