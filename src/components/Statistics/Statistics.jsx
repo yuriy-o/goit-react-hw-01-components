7e51090ac26680af40f2b93b8d2aafca0cfc9c41
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-01-16 00:15:01 +0200

    second homework

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,54 @@
+import {
+  Section,
+  Title,
+  List,
+  ListItem,
+  Label,
+  Percentage,
+} from './Statistics.styled';
+
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ title, data }) => {
+  const dataLength = data.length;
+  console.log('dataLength', dataLength);
+  return (
+    <Section>
+      <Title>{title}</Title>
+
+      <List>
+        {data.map(({ id, label, percentage }) => {
+          return (
+            <ListItem
+              key={id}
+              style={{
+                backgroundColor: getRandomHexColor(),
+                width: 100 / dataLength + '%',
+              }}
+            >
+              <Label>{label}</Label>
+              <Percentage>{percentage}%</Percentage>
+            </ListItem>
+          );
+        })}
+      </List>
+    </Section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  width: 345px;
+  min-height: 100%;
+
+  text-align: center;
+  padding-top: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+  margin-bottom: 40px;
+
+  border-radius: 5px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`;
+
+export const Title = styled.h2`
+  font-size: 28px;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  justify-content: space-around;
+
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+export const ListItem = styled.li`
+  display: flex;
+  flex-direction: column;
+
+  padding: 10px;
+  gap: 10px 5px;
+
+  color: white;
+`;
+
+export const Label = styled.span``;
+
+export const Percentage = styled.span``;
